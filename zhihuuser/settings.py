a94edfeb4da90ee731c23d9f861dc62ed4c769b8
wuyuqiang <1018656696@qq.com>
2017-12-27 15:24:39 +0800

    修改优先级

--- zhihuuser/settings.py
+++ zhihuuser/settings.py
@@ -67,7 +67,7 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'zhihuuser.pipelines.MongoPipeline': 300,
+   'zhihuuser.pipelines.MongoPipeline': 299,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
